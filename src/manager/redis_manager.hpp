75890f7b0adcb63f85296dc753a1d72e201a3351
Ramsay <ramsayleung@gmail.com>
2020-05-07 13:04:15 +0800

    将redis_manager 获取结果改为optional

--- src/manager/redis_manager.hpp
+++ src/manager/redis_manager.hpp
@@ -2,26 +2,27 @@
 #define REDIS_MANAGER_HPP
 #include <hiredis/hiredis.h>
 
+#include <optional>
 #include <string>
 namespace fcdeduction {
 namespace manager {
 class RedisManager {
- public:
-  explicit RedisManager(const std::string &hostname, const int port) {
-    context = redisConnect(hostname.c_str(), port);
-  }
-  ~RedisManager() { redisFree(context); }
-  // 获取指定key的对应的value, 如果为空, 返回nullptr
-  std::string get(const std::string &key);
+   public:
+    explicit RedisManager(const std::string &hostname, const int port) {
+        context = redisConnect(hostname.c_str(), port);
+    }
+    ~RedisManager() { redisFree(context); }
+    // 获取指定key的对应的value, 如果为空, 返回nullptr
+    std::optional<std::string> get(const std::string &key);
 
-  // 设置key-value.
-  void set(const std::string &key, const std::string &value);
+    // 设置key-value.
+    void set(const std::string &key, const std::string &value);
 
-  // 删除指定key-value
-  void del(const std::string &key);
+    // 删除指定key-value
+    void del(const std::string &key);
 
- private:
-  redisContext *context;
+   private:
+    redisContext *context;
 };
 
 }  // namespace manager
--- tests/redis_manager_test.cc
+++ tests/redis_manager_test.cc
@@ -5,10 +5,17 @@
 #include "gtest/gtest.h"
 
 TEST(RedisManagerGet, ReturnHello) {
-  fcdeduction::manager::RedisManager redisManager("127.0.0.1", 6379);
-  redisManager.del("hello");
-  redisManager.set("hello", "202005051647");
-  auto value = redisManager.get("hello");
-  std::cout << "value: " << value << '\n';
-  EXPECT_EQ(value, "202005051647");
+    fcdeduction::manager::RedisManager redisManager("127.0.0.1", 6379);
+    redisManager.del("hello");
+    redisManager.set("hello", "202005051647");
+    auto value = redisManager.get("hello");
+    if (value.has_value()) {
+        std::cout << "value: " << value.value() << '\n';
+    }
+    value = redisManager.get("notExist");
+    if (!value.has_value()) {
+        std::cout << "can not found value "
+                  << "\n";
+    }
+    EXPECT_EQ(*value, "202005051647");
 }
