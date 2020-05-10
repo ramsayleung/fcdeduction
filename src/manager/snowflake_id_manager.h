11bf8d2b4fff134d0ad63e4379920bb2bc978398
Ramsay <ramsayleung+github@gmail.com>
2020-05-10 19:56:32 +0800

    修复id_manager 编译失败问题

--- src/manager/snowflake_id_manager.h
+++ src/manager/snowflake_id_manager.h
@@ -1,71 +1,76 @@
 #ifndef MANAGER_SNOWFLAKE_ID_MANAGER_H
 #define MANAGER_SNOWFLAKE_ID_MANAGER_H
-// 参考于 https://github.com/sniper00/snowflake-cpp/blob/master/snowflake.hpp
-namespace fcdeduction {
-namespace manager {
 #include <stdint.h>
 #include <sys/time.h>
 
 #include <mutex>
 #include <stdexcept>
+// 参考于 https://github.com/sniper00/snowflake-cpp/blob/master/snowflake.hpp
+namespace fcdeduction {
+namespace manager {
 
 class SnowFlakeIdManager {
-   private:
-    static const uint64_t start_stmp_ = 1480166465631;
-    static const uint64_t sequence_bit_ = 12;
-    static const uint64_t machine_bit_ = 5;
-    static const uint64_t datacenter_bit_ = 5;
+ private:
+  static const uint64_t start_stmp_ = 1480166465631;
+  static const uint64_t sequence_bit_ = 12;
+  static const uint64_t machine_bit_ = 5;
+  static const uint64_t datacenter_bit_ = 5;
 
-    static const uint64_t max_datacenter_num_ = -1 ^ (uint64_t(-1) << datacenter_bit_);
-    static const uint64_t max_machine_num_ = -1 ^ (uint64_t(-1) << machine_bit_);
-    static const uint64_t max_sequence_num_ = -1 ^ (uint64_t(-1) << sequence_bit_);
+  static const uint64_t max_datacenter_num_ =
+      -1 ^ (uint64_t(-1) << datacenter_bit_);
+  static const uint64_t max_machine_num_ = -1 ^ (uint64_t(-1) << machine_bit_);
+  static const uint64_t max_sequence_num_ =
+      -1 ^ (uint64_t(-1) << sequence_bit_);
 
-    static const uint64_t machine_left = sequence_bit_;
-    static const uint64_t datacenter_left = sequence_bit_ + machine_bit_;
-    static const uint64_t timestmp_left = sequence_bit_ + machine_bit_ + datacenter_bit_;
+  static const uint64_t machine_left = sequence_bit_;
+  static const uint64_t datacenter_left = sequence_bit_ + machine_bit_;
+  static const uint64_t timestmp_left =
+      sequence_bit_ + machine_bit_ + datacenter_bit_;
 
-    uint64_t datacenterId;
-    uint64_t machineId;
-    uint64_t sequence;
-    uint64_t lastStmp;
+  uint64_t datacenterId;
+  uint64_t machineId;
+  uint64_t sequence;
+  uint64_t lastStmp;
 
-    std::mutex mutex_;
+  std::mutex mutex_;
 
-    uint64_t getNextMill() {
-        uint64_t mill = getNewstmp();
-        while (mill <= lastStmp) {
-            mill = getNewstmp();
-        }
-        return mill;
+  uint64_t getNextMill() {
+    uint64_t mill = getNewstmp();
+    while (mill <= lastStmp) {
+      mill = getNewstmp();
     }
+    return mill;
+  }
 
-    uint64_t getNewstmp() {
-        struct timeval tv;
-        gettimeofday(&tv, NULL);
+  uint64_t getNewstmp() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
 
-        uint64_t time = tv.tv_usec;
-        time /= 1000;
-        time += (tv.tv_sec * 1000);
-        return time;
+    uint64_t time = tv.tv_usec;
+    time /= 1000;
+    time += (tv.tv_sec * 1000);
+    return time;
+  }
+
+ public:
+  SnowFlakeIdManager(int datacenter_Id, int machine_Id) {
+    if ((uint64_t)datacenter_Id > max_datacenter_num_ || datacenter_Id < 0) {
+      // LLOG(ERRO) << "datacenterId can't be greater than max_datacenter_num_
+      // or less than 0";
+      exit(0);
     }
-
-   public:
-    SnowFlakeIdManager(int datacenter_Id, int machine_Id) {
-        if ((uint64_t)datacenter_Id > max_datacenter_num_ || datacenter_Id < 0) {
-            // LLOG(ERRO) << "datacenterId can't be greater than max_datacenter_num_ or less than 0";
-            exit(0);
-        }
-        if ((uint64_t)machine_Id > max_machine_num_ || machine_Id < 0) {
-            // LLOG(ERRO) << "machineId can't be greater than max_machine_num_or less than 0";
-            exit(0);
-        }
-        datacenterId = datacenter_Id;
-        machineId = machine_Id;
-        sequence = 0L;
-        lastStmp = 0L;
+    if ((uint64_t)machine_Id > max_machine_num_ || machine_Id < 0) {
+      // LLOG(ERRO) << "machineId can't be greater than max_machine_num_or less
+      // than 0";
+      exit(0);
     }
+    datacenterId = datacenter_Id;
+    machineId = machine_Id;
+    sequence = 0L;
+    lastStmp = 0L;
+  }
 
-    uint64_t nextId();
+  uint64_t nextId();
 };
 
 }  // namespace manager
--- tests/snowflake_id_manager_test.cc
+++ tests/snowflake_id_manager_test.cc
@@ -1,16 +1,31 @@
 #include "src/manager/snowflake_id_manager.h"
 
 #include <cstdint>
+#include <iostream>
 #include <set>
+#include <thread>
 
 #include "gtest/gtest.h"
+std::set<int64_t> idSet;
+void test(fcdeduction::manager::SnowFlakeIdManager &idManager) {
+  for (int i = 0; i < 1000000; i++) {
+    auto id = idManager.nextId();
+    bool contains = idSet.find(id) != idSet.end();
+    EXPECT_TRUE(!contains);
+    idSet.insert(id);
+    std::cout << "id from thread: " << id << '\n';
+  }
+}
+
 TEST(MANAGER_TEST, NEXT_ID_TEST) {
-    fcdeduction::manager::SnowFlakeIdManager idManager(1024, 1000);
-    std::set<int64_t> idSet;
-    for (int i = 0; i < 1000000; i++) {
-        auto id = idManager.nextId();
-        bool contains = idSet.find(id) != idSet.end();
-        EXPECT_TRUE(!contains);
-        idSet.insert(id);
-    }
+  fcdeduction::manager::SnowFlakeIdManager idManager(1024, 1000);
+  std::thread t1(test, std::ref(idManager));
+  for (int i = 0; i < 1000000; i++) {
+    auto id = idManager.nextId();
+    bool contains = idSet.find(id) != idSet.end();
+    EXPECT_TRUE(!contains);
+    idSet.insert(id);
+    std::cout << "id: " << id << '\n';
+  }
+  t1.join();
 }
